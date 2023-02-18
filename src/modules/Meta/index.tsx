1de4d34e04a839bb51ca4d38bbc2c29fcafd8702
RichardPickman <drezzerock@gmail.com>
2023-02-18 19:22:23 +0100

    feat: add Meta module

---
+++ src/modules/Meta/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from "next/head";
+
+function Meta() {
+  return (
+    <Head>
+      <title>Архпотолки - натяжные потолки любой сложности!</title>
+      <link rel="icon" href="/favicon.ico" key="favicon" />
+      <meta
+        property="og:description"
+        content="Установка натяжных потолков любой сложности в Архангельске и области"
+      />
+    </Head>
+  );
+}
+
+export default Meta;
