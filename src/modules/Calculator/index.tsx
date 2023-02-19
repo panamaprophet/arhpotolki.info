01feee1a4afd14c8f91bdf666da54e25a65d0271
RichardPickman <drezzerock@gmail.com>
2023-02-19 21:54:34 +0100

    add states and constants

--- src/modules/Calculator/index.tsx
+++ src/modules/Calculator/index.tsx
@@ -1,21 +1,45 @@
-import React from "react";
-import styles from "./styles.module.css";
-import Title from "../../components/Title";
-import Preview from "./components/Preview";
-import Controls from "./components/Controls";
+import React, { useState } from 'react';
+import styles from './styles.module.css';
+import Title from '../../components/Title';
+import Preview from './components/Preview';
+import Controls from './components/Controls';
+
+const initialColors = {
+    roof: '#ffffff',
+    walls: '#a5c8db',
+    floor: '#626c6e',
+};
+
+export const materials = {
+    gloss: 'Глянец',
+    mate: 'Матовый',
+    satine: 'Сатин',
+    clouds: 'Облака',
+};
 
 function Calculator() {
-  return (
-    <section id="calculator" className="layout">
-      <Title>Рассчитать стоимость:</Title>
-      <div className={styles.visualizer}>
-        <div className={styles.visualizer__view}>
-          <Preview />
-          <Controls />
-        </div>
-      </div>
-    </section>
-  );
+    const [colors, setColors] = useState(initialColors);
+    const [material, setMaterial] = useState(materials.gloss);
+
+    const handleColors = (data) => setColors(data);
+    const handleMaterialChange = (data) => setMaterial(data);
+
+    return (
+        <section id="calculator" className="layout">
+            <Title>Рассчитать стоимость:</Title>
+            <div className={styles.visualizer}>
+                <div className={styles.visualizer__view}>
+                    <Preview colors={colors} material={material} />
+                    <Controls
+                        colors={colors}
+                        material={material}
+                        onColorChange={handleColors}
+                        onMaterialChange={handleMaterialChange}
+                    />
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Calculator;
