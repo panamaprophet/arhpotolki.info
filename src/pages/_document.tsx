eaa9ed7b798e295d625dcba426aef4d9feb84ab7
RichardPickman <drezzerock@gmail.com>
2023-02-19 18:01:44 +0100

    move <title> to _app due to pre-render issues

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <title>Архпотолки - натяжные потолки любой сложности!</title>
         <link rel="icon" href="/favicon.ico" key="favicon" />
         <meta
           property="og:description"
