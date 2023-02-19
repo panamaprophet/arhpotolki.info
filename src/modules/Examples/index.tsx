eb53b6ebb67c1755a8648ff6afd868d94e023f6a
RichardPickman <drezzerock@gmail.com>
2023-02-19 15:38:47 +0100

    feat: add filters

--- src/modules/Examples/index.tsx
+++ src/modules/Examples/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import BlockTitle from "../../components/BlockTitle";
 import buttons from "./buttons";
 import Button from "./components/Button";
@@ -6,31 +6,77 @@ import Image from "./components/Image";
 import styles from "./styles.module.css";
 import gallery from "./gallery";
 
-function Examples() {
-  const handleClick = () => {};
+type Button = {
+  category: string;
+};
 
-  return (
-    <section id="examples" className={`${styles.examples} layout`}>
-      <BlockTitle>
-        Наши работы (<span>все</span> или <span>по категориям</span>)
-      </BlockTitle>
+type FilterType = "all" | "category";
+
+function Examples() {
+  const [currentButton, setCurrentButton] = useState(buttons[0]);
+  const [filterType, setFilterType] = useState("all");
+
+  const filteredGallery = gallery.filter(
+    (img) => img.category === currentButton.category
+  );
+
+  const handleFilter = (filter: FilterType) => setFilterType(filter);
+
+  const handleClick = (button: Button) => setCurrentButton(button);
+
+  const FullGallery = () => (
+    <ul className={styles.gallery}>
+      {gallery.map((img) => (
+        <li className={styles.gallery__item}>
+          <Image key={img.path} path={img.path} />
+        </li>
+      ))}
+    </ul>
+  );
+
+  const GalleryWithFilters = () => (
+    <>
       <ul className={styles.filter}>
-        {buttons.map((button) => (
+        {buttons.map((button: Button) => (
           <Button
-            key={button.text}
-            text={button.text}
+            key={button.category}
+            button={button}
             onClick={handleClick}
-            isActive={true}
+            isActive={button === currentButton}
           />
         ))}
       </ul>
       <ul className={styles.gallery}>
-        {gallery.map((img) => (
+        {filteredGallery.map((img) => (
           <li className={styles.gallery__item}>
             <Image key={img.path} path={img.path} />
           </li>
         ))}
       </ul>
+    </>
+  );
+
+  return (
+    <section id="examples" className={`${styles.examples} layout`}>
+      <BlockTitle>
+        Наши работы (
+        <span
+          className={styles.filter__type}
+          onClick={() => handleFilter("all")}
+        >
+          все
+        </span>{" "}
+        или{" "}
+        <span
+          className={styles.filter__type}
+          onClick={() => handleFilter("category")}
+        >
+          по категориям
+        </span>
+        )
+      </BlockTitle>
+      {filterType === "all" && <FullGallery />}
+      {filterType === "category" && <GalleryWithFilters />}
     </section>
   );
 }
