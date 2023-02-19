08633be01b587e1b2dfdb36e6d3798f27f035d30
RichardPickman <drezzerock@gmail.com>
2023-02-19 13:08:28 +0100

    feat: add Certificates module

---
+++ src/modules/Certificates/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./styles.module.css";
+import BlockTitle from "../../components/BlockTitle";
+import certs from "./certs";
+
+function Certificates() {
+  return (
+    <section id="certificates" className="layout">
+      <BlockTitle>Сертификаты соответствия:</BlockTitle>
+      <ul className={styles.certificates__list}>
+        {certs.map((cert) => (
+          <li className={styles.certificates__list__item}>
+            <img src={cert.path} />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default Certificates;
