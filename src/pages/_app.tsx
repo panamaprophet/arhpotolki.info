94531b0250e1db4308099276316d123b0690d2fa
RichardPickman <drezzerock@gmail.com>
2023-02-19 18:01:44 +0100

    move <title> to _app due to pre-render issues

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,19 @@
 // import type { AppProps } from 'next/app';
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
-import '../styles/global.css';
+import "../styles/global.css";
 
-export default function App({ Component, pageProps: { session, ...pageProps } }) {
-    return (
-        <SessionProvider session={session}>
-            <Component {...pageProps} />
-        </SessionProvider>
-    );
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Head>
+        <title>Архпотолки - натяжные потолки любой сложности!</title>
+      </Head>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
