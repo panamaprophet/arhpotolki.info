57de6c6047caacbab97f509665164a9a04d6ad47
pp <pp@rawsound.ru>
2023-02-20 10:29:04 +0100

    add kv settings api

---
+++ src/resolvers/settings/index.ts
@@ -0,0 +1,33 @@
+import { PutItemCommand, DeleteItemCommand, QueryCommand } from '@aws-sdk/client-dynamodb';
+import { marshall, unmarshall } from '@aws-sdk/util-dynamodb';
+import { client as db } from '../../services/db';
+
+
+const SETTINGS_TABLE = String(process.env.SETTINGS_TABLE);
+
+
+export const setSetting = async ({ key, value }) => {
+    const result = await db.send(new PutItemCommand({
+        TableName: SETTINGS_TABLE,
+        Item: marshall({ key, value }),
+    }));
+
+    return result.$metadata.httpStatusCode === 200 ? key : null;
+};
+
+export const removeSetting = async (key: string) => {
+    const result = await db.send(new DeleteItemCommand({
+        TableName: SETTINGS_TABLE,
+        Key: marshall({ key }),
+    }));
+
+    return result.$metadata.httpStatusCode === 200;
+};
+
+export const getSettings = async () => {
+    const result = await db.send(new QueryCommand({
+        TableName: SETTINGS_TABLE,
+    }))
+
+    return result.Count > 0 ? result.Items.map(item => unmarshall(item)) : null;
+};
