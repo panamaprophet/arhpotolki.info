d6c11460a02c60e411210d34a1bd17e602c48f18
RichardPickman <drezzerock@gmail.com>
2023-02-21 18:30:15 +0100

    add temporary constants

---
+++ src/constants/index.tsx
@@ -0,0 +1,9 @@
+export const initialColors = {
+    roof: '#ffffff',
+    walls: '#a5c8db',
+    floor: '#626c6e',
+};
+
+export const materials = ['Глянец', 'Матовый', 'Сатин', 'Облака'];
+
+export const budgets = ['Эконом (КНР)', 'Премиум (Германия)'];
