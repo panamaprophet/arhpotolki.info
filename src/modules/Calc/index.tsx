162704a532c438c2a173835f892fac096ec48bf7
RichardPickman <drezzerock@gmail.com>
2023-02-22 16:02:45 +0100

    update props

--- src/modules/Calc/index.tsx
+++ src/modules/Calc/index.tsx
@@ -1,15 +1,22 @@
 import Title from '../../components/Title';
 import Calculator from '../../components/Calculator';
-import { budgets, materials } from '../../constants';
+import { materials } from '../../constants';
+import { colorPrice, prices } from './mock';
 
 function Calc() {
+    const initialPrices = {
+        square: 700,
+        light: 350,
+    };
     return (
         <section id="calculator" className="layout">
             <Title>Рассчитать стоимость:</Title>
             <Calculator
                 onCalc={() => {}}
                 materials={materials}
-                types={budgets}
+                prices={prices}
+                lightPrice={initialPrices.light}
+                colorPrice={colorPrice}
             />
         </section>
     );
