25db21c743d62f4c8663ce493d0ce7f0c9f85247
RichardPickman <drezzerock@gmail.com>
2023-02-23 11:35:22 +0100

    update mocks

--- src/modules/Calc/index.tsx
+++ src/modules/Calc/index.tsx
@@ -1,13 +1,8 @@
 import Title from '../../components/Title';
 import Calculator from '../../components/Calculator';
-import { materials } from '../../constants';
-import { colorPrice, prices } from './mock';
+import { colorPrice, lightPrice, prices, materials } from './mock';
 
 function Calc() {
-    const initialPrices = {
-        square: 700,
-        light: 350,
-    };
     return (
         <section id="calculator" className="layout">
             <Title>Рассчитать стоимость:</Title>
@@ -15,7 +10,7 @@ function Calc() {
                 onCalc={() => {}}
                 materials={materials}
                 prices={prices}
-                lightPrice={initialPrices.light}
+                lightPrice={lightPrice}
                 colorPrice={colorPrice}
             />
         </section>
