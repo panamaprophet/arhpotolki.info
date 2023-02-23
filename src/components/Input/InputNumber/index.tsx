35fece8f08f20013ebf41fc9ab96567baef599e1
RichardPickman <drezzerock@gmail.com>
2023-02-23 11:43:18 +0100

    handle readonly in number

--- src/components/Input/InputNumber/index.tsx
+++ src/components/Input/InputNumber/index.tsx
@@ -4,20 +4,14 @@ import styles from './styles.module.css';
 type InputProps = {
     value: number;
     onChange?: (event) => void;
-    readonly?: boolean;
 };
 
-export const InputNumber = ({
-    value,
-    onChange,
-    readonly,
-    ...props
-}: InputProps) => {
+export const InputNumber = ({ value, onChange, ...props }: InputProps) => {
     return (
         <input
             type="number"
             value={String(value)}
-            readOnly={Boolean(readonly)}
+            readOnly={onChange ? false : true}
             className={styles.input__field}
             onChange={event => onChange(event.target.value)}
             {...props}
