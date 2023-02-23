332c6ff967e5757a7eb1ed646de607702c7f2e6b
RichardPickman <drezzerock@gmail.com>
2023-02-23 15:30:59 +0100

    remove carousel to carousel component

--- src/modules/Feedback/index.tsx
+++ src/modules/Feedback/index.tsx
@@ -1,49 +1,32 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './styles.module.css';
 import Title from '../../components/Title';
 import mock from './mock';
 import FeedbackItem from './components/FeedbackItem';
+import Carousel from '../../components/Carousel';
 
 function Feedbacks() {
-  const [persons, setPersons] = useState(mock);
-  const [currentPersonIndex, setCurrentPersonIndex] = useState(0);
+    const persons = mock;
 
-  const handleNextPerson = () => {
-    if (currentPersonIndex === persons.length - 1) {
-      setCurrentPersonIndex(0);
-    } else {
-      setCurrentPersonIndex((prev) => prev + 1);
-    }
-  };
-  const handlePrevPerson = () => {
-    if (currentPersonIndex === 0) {
-      setCurrentPersonIndex(persons.length - 1);
-    } else {
-      setCurrentPersonIndex((prev) => prev - 1);
-    }
-  };
-
-  return (
-    <section id="feedbacks">
-      <div className="layout">
-        <Title>Отзывы:</Title>
-        <div className={styles.responses}>
-          <div className={styles.responses__prev} onClick={handlePrevPerson}>
-            «
-          </div>
-          <div className={styles.responses__list}>
-            <FeedbackItem person={persons[currentPersonIndex]} />
-          </div>
-          <div className={styles.responses__next} onClick={handleNextPerson}>
-            »
-          </div>
-        </div>
-        <div className={styles.leave__response}>
-          <span className={styles.leave__response__title}>Оставить отзыв</span>
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section id="feedbacks">
+            <div className="layout">
+                <Title>Отзывы:</Title>
+                <div className={styles.responses}>
+                    <Carousel items={persons}>
+                        {persons.map(item => (
+                            <FeedbackItem key={item.name} person={item} />
+                        ))}
+                    </Carousel>
+                </div>
+                <div className={styles.leave__response}>
+                    <span className={styles.leave__response__title}>
+                        Оставить отзыв
+                    </span>
+                </div>
+            </div>
+        </section>
+    );
 }
 
 export default Feedbacks;
