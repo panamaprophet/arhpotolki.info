4cd58f61f6ca43ffb5fb520505814877f0f92342
RichardPickman <drezzerock@gmail.com>
2023-02-23 11:34:25 +0100

    remove constants

--- src/constants/index.tsx
+++
@@ -1,9 +0,0 @@
-export const initialColors = {
-    roof: '#ffffff',
-    walls: '#a5c8db',
-    floor: '#626c6e',
-};
-
-export const materials = ['Глянец', 'Матовый', 'Сатин', 'Облака'];
-
-export const budgets = ['Эконом (КНР)', 'Премиум (Германия)'];
