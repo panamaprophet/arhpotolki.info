830ccc04b0c8bf4fe763f6744a0a5a7306b17e03
pp <pp@rawsound.ru>
2023-02-23 17:34:33 +0100

    rename input's setState functions

--- src/components/Input/InputList/index.tsx
+++ src/components/Input/InputList/index.tsx
@@ -1,25 +1,25 @@
 import { FocusEventHandler, KeyboardEventHandler, useState } from 'react';
 
 
-export const InputList = ({ value, onChange }) => {
-    const [newValue, setValue] = useState(value);
+export const InputList = ({ value: initialValue, onChange }) => {
+    const [value, setValue] = useState(initialValue);
 
     const onBlur: FocusEventHandler<HTMLInputElement> = () => {
-        if (value !== newValue) {
-            onChange(newValue);
+        if (initialValue !== value) {
+            onChange(value);
         }
     };
 
     const onKeyDown: KeyboardEventHandler<HTMLInputElement> = (event) => {
-        if (event.code === 'Enter' && value !== newValue) {
-            onChange(newValue);
+        if (event.code === 'Enter' && initialValue !== value) {
+            onChange(value);
         }
     };
 
     return (
         <input
             type="text"
-            defaultValue={newValue.join(' ')}
+            defaultValue={value.join(' ')}
             onChange={event => setValue(event.target.value.split(' '))}
             onKeyDown={onKeyDown}
             onBlur={onBlur}
