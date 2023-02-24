8c40cbb4fd4b528413256ef234ceca9493d6d09b
RichardPickman <drezzerock@gmail.com>
2023-02-24 21:17:30 +0100

    remove wrapper, change styles, update FeedbackItem props

--- src/modules/Feedback/index.tsx
+++ src/modules/Feedback/index.tsx
@@ -4,27 +4,20 @@ import Title from '../../components/Title';
 import mock from './mock';
 import FeedbackItem from './components/FeedbackItem';
 import Carousel from '../../components/Carousel';
+import { Feedback } from '../../types';
 
 function Feedbacks() {
-    const persons = mock;
+    const persons: Feedback[] = mock;
 
     return (
         <section id="feedbacks">
-            <div className="layout">
-                <Title>Отзывы:</Title>
-                <div className={styles.responses}>
-                    <Carousel items={persons}>
-                        {persons.map(item => (
-                            <FeedbackItem key={item.name} person={item} />
-                        ))}
-                    </Carousel>
-                </div>
-                <div className={styles.leave__response}>
-                    <span className={styles.leave__response__title}>
-                        Оставить отзыв
-                    </span>
-                </div>
-            </div>
+            <Title>Отзывы:</Title>
+            <Carousel>
+                {persons.map(item => (
+                    <FeedbackItem key={item.id} {...item} />
+                ))}
+            </Carousel>
+            <button>Оставить отзыв</button>
         </section>
     );
 }
