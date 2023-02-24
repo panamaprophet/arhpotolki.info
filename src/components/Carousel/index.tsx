20e87e32f6092a88ddf894c0412195a5b557286c
RichardPickman <drezzerock@gmail.com>
2023-02-24 22:07:40 +0100

    change onForward && onBackward to single setState with ternary operator

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,11 +1,11 @@
-import React, { Children, ReactNode, useMemo, useRef, useState } from 'react';
+import { Children, ReactNode, useMemo, useRef, useState } from 'react';
 import styles from './styles.module.css';
 
 interface Props {
     children: ReactNode;
 }
 
-const Carousel = ({ children }: Props) => {
+export const Carousel = ({ children }: Props) => {
     const ref = useRef<HTMLDivElement>(null);
     const lastIndex = Children.count(children) - 1;
     const [currentIndex, setIndex] = useState(0);
@@ -19,21 +19,8 @@ const Carousel = ({ children }: Props) => {
         return containerWidth * currentIndex;
     }, [ref, currentIndex]);
 
-    const onForward = () => {
-        if (lastIndex === currentIndex) {
-            setIndex(0);
-        } else {
-            setIndex(currentIndex + 1);
-        }
-    };
-
-    const onBackward = () => {
-        if (currentIndex === 0) {
-            setIndex(lastIndex);
-        } else {
-            setIndex(currentIndex - 1);
-        }
-    };
+    const onForward = () => setIndex(lastIndex === currentIndex ? 0 : currentIndex + 1);
+    const onBackward = () => setIndex(currentIndex === 0 ? lastIndex : currentIndex - 1);
 
     return (
         <div className={styles.root}>
@@ -42,10 +29,7 @@ const Carousel = ({ children }: Props) => {
                     «
                 </div>
                 <div className={styles.viewport} ref={ref}>
-                    <div
-                        className={styles.list}
-                        style={{ transform: `translateX(-${offset}px)` }}
-                    >
+                    <div className={styles.list} style={{ transform: `translateX(-${offset}px)` }}>
                         {children}
                     </div>
                 </div>
@@ -56,5 +40,3 @@ const Carousel = ({ children }: Props) => {
         </div>
     );
 };
-
-export default Carousel;
