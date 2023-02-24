585156dacec88f900325806662ec1078bce15cff
pp <pp@rawsound.ru>
2023-02-24 17:47:11 +0100

    add Text component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Calc from '../modules/Calc';
-import Description from '../modules/Description';
 import Gallery from '../modules/Gallery';
 import Header from '../modules/Header';
 import Separator from '../modules/Separator';
@@ -9,7 +8,9 @@ import { OrderForm } from '../components/OrderForm';
 import { List } from '../components/List';
 import Image from 'next/image';
 import { Card } from '../components/Card';
+
 import { certificates, steps, features, feedbacks } from './index.mock';
+import { Text } from '../components/Text';
 import { Carousel } from '../components/Carousel';
 import { FeedbackItem } from '../components/FeedbackItem/FeedbackItem';
 
@@ -20,7 +21,12 @@ const App = () => {
     return (
         <>
             <Header />
-            <Description />
+
+            <Text>
+                <p>Компания «АрхПотолки» на про­тя­же­нии шес­ти лет ус­пеш­но за­ни­ма­ет­ся про­да­жей и ус­та­нов­кой на­тяж­ных по­тол­ков, на тер­ри­то­рии всей Ар­хан­гель­ской об­ла­сти.</p>
+                <p>Сво­им глав­ным дос­ти­же­ни­ем мы счи­та­ем ин­ди­ви­ду­аль­ный под­ход к каж­до­му кли­ен­ту и пер­во­класс­ное ка­чес­тво про­из­во­ди­мой на­ми ра­бо­ты.</p>
+                <p>Од­но из глав­ных пре­и­му­ществ на­шей ком­па­нии, яв­ля­ет­ся сер­вис­ное об­слу­жи­ва­ние. Ведь на про­тя­же­нии всей га­ра­нтии мы за­бо­тим­ся о каж­дом ус­та­нов­лен­ном на­ми по­тол­ке, будь то бюд­жет­ный или экс­клю­зив­ный по­то­лок.</p>
+            </Text>
 
             <section id="advantages" style={{ background: '#f7a136' }}>
                 <Title>Только у нас:</Title>
