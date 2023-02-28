9eecc4ecb2eb7fe2b2ae81bf78c365c4bde4bd86
RichardPickman <drezzerock@gmail.com>
2023-02-28 22:22:33 +0100

    set links from props

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,37 +1,16 @@
 import styles from './styles.module.css';
 
-function Menu({ onClick }) {
+function Menu({ links, onClick }) {
   return (
     <div className={styles.navigation}>
-      <div className={styles.closeButton} onClick={onClick}>
-        X
-      </div>
-      <ul className={styles.menu} onClick={onClick}>
-        <li className={styles.item}>
-          <a href="#advantages" className={styles.link}>
-            Преимущества
-          </a>
+      <ul className={styles.menu}>
+        {links.map((item) => 
+          <li key={item.id} className={styles.item} onClick={onClick}>
+            <a href={item.link} className={styles.link}>
+                {item.title}
+            </a>
         </li>
-        <li className={styles.item}>
-          <a href="#examples" className={styles.link}>
-            Наши работы
-          </a>
-        </li>
-        <li className={styles.item}>
-          <a href="#calculator" className={styles.link}>
-            Расчёт стоимости
-          </a>
-        </li>
-        <li className={styles.item}>
-          <a href="#application" className={styles.link}>
-            Оставить заявку
-          </a>
-        </li>
-        <li className={styles.item}>
-          <a href="#footer" className={styles.link}>
-            Контакты
-          </a>
-        </li>
+        )}
       </ul>
     </div>
   );
