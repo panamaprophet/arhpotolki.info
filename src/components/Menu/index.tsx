e5eef37008313f604d403a62d49fd931673e60b9
RichardPickman <drezzerock@gmail.com>
2023-02-28 23:15:31 +0100

    use id as link

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,11 +2,11 @@ import styles from './styles.module.css';
 
 function Menu({ links, onClick }) {
   return (
-    <div className={styles.navigation}>
+    <div className={styles.navigation} onClick={onClick}>
       <ul className={styles.menu}>
         {links.map((item) => 
-          <li key={item.id} className={styles.item} onClick={onClick}>
-            <a href={item.link} className={styles.link}>
+          <li key={item.id} className={styles.item} >
+            <a href={item.id} className={styles.link}>
                 {item.title}
             </a>
         </li>
