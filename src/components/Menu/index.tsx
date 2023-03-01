6baec8910a3d788919d29ccee7375c22ae592943
pp <pp@rawsound.ru>
2023-03-01 16:32:33 +0100

    cleanup the menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,15 +2,11 @@ import styles from './styles.module.css';
 
 
 export const Menu = ({ links, onClick }) => (
-    <div className={styles.navigation} onClick={onClick}>
-        <ul className={styles.menu}>
-            {links.map((item) =>
-                <li key={item.id} className={styles.item} >
-                    <a href={`#${item.id}`} className={styles.link}>
-                      {item.title}
-                    </a>
-                </li>
-            )}
-        </ul>
-    </div>
+    <nav className={styles.root} onClick={onClick}>
+        {links.map(item => (
+            <a key={item.id} href={`#${item.id}`} className={styles.link}>
+                {item.title}
+            </a>
+        ))}
+    </nav>
 );
