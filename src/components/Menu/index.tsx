0eae4deefeded64c7875418b3c202cadf53905bb
pp <pp@rawsound.ru>
2023-03-01 14:48:24 +0100

    move gallery to components, fix menu links and put feedbackform in modal

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,17 +1,16 @@
 import styles from './styles.module.css';
 
-export const Menu = ({ links, onClick }) => {
-  return (
+
+export const Menu = ({ links, onClick }) => (
     <div className={styles.navigation} onClick={onClick}>
-      <ul className={styles.menu}>
-        {links.map((item) => 
-          <li key={item.id} className={styles.item} >
-            <a href={item.id} className={styles.link}>
-                {item.title}
-            </a>
-        </li>
-        )}
-      </ul>
+        <ul className={styles.menu}>
+            {links.map((item) =>
+                <li key={item.id} className={styles.item} >
+                    <a href={`#${item.id}`} className={styles.link}>
+                      {item.title}
+                    </a>
+                </li>
+            )}
+        </ul>
     </div>
-  );
-}
+);
