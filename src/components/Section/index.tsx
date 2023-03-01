0e75d96ff8d0ce5a7a8a176258dab29cfe1be4da
RichardPickman <drezzerock@gmail.com>
2023-03-02 00:02:31 +0100

    add wrapper

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -3,7 +3,9 @@ import styles from './styles.module.css';
 export const Section = ({ id, children, style = {} }) => {
   return (
     <section id={id} className={styles.root} style={style}>
+      <div className={styles.wrapper}>
         {children}
+      </div>
     </section>
   )
 }
