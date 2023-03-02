2de21c1b1422e7192e29775f2e5c8e2a4c99bf5e
RichardPickman <drezzerock@gmail.com>
2023-03-02 18:11:30 +0100

    add Yandex map component

---
+++ src/components/Map/index.tsx
@@ -0,0 +1,5 @@
+import styles from './styles.module.css'
+
+export const Map = () => (
+    <iframe className={styles.root} src="https://yandex.com/map-widget/v1/?um=constructor%3AG6nVYiHn2l8NpWx4mskMqjtmIWaAwM2V&amp;source=constructor" width="100%" height="100%" />
+)
