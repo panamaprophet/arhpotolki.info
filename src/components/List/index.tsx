6e1244da533ddc445eb5c2340efd049ee3f9d555
RichardPickman <drezzerock@gmail.com>
2023-03-03 12:14:48 +0100

    add margin for <ul> tag, add list style for <li> tag

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -2,16 +2,46 @@ import { Children, ReactNode } from 'react';
 import { cx } from '../../helpers';
 import styles from './index.module.css';
 
+type list = 'default' | 'numeric' | 'bullet' | 'circle';
 
 interface Props {
+    children: ReactNode,
     nowrap?: boolean,
     align?: string,
-    children: ReactNode,
+    margin?: string | number,
+    flexFlow?: string,
+    listType?: list,
 }
 
 
-export const List = ({ children, align = 'evenly', nowrap = false }: Props) => (
-    <ul className={cx(styles.root, nowrap && styles.root_nowrap, styles[`align_${align}`])}>
-        {Children.map(children, (child, index) => <li key={index} className={styles.listItem}>{child}</li>)}
+export const List = ({
+    children, 
+    align = 'evenly', 
+    margin = 'auto',
+    nowrap = false, 
+    flexFlow = 'row',
+    listType='default', 
+}: Props) => (
+    <ul 
+        className={
+            cx(
+                styles.root, 
+                nowrap && styles.root_nowrap,
+                styles[`flexFlow_${flexFlow}`],
+                styles[`align_${align}`],
+            )}
+        style={{ margin: margin }}
+    >
+        {Children.map(children, (child, index) => (
+            <li 
+                key={index} 
+                className={
+                    cx(
+                        styles[`listType_${listType}`]
+                    )}
+            >
+                {child}
+            </li>
+        ))}
     </ul>
 );
