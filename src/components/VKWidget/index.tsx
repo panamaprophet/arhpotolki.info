976128b6ed5ff22378a53cce265581c32153622b
RichardPickman <drezzerock@gmail.com>
2023-03-04 18:00:37 +0100

    refactor widget component

--- src/components/VKWidget/index.tsx
+++ src/components/VKWidget/index.tsx
@@ -1,14 +1,18 @@
 import Script from 'next/script';
 import styles from './styles.module.css';
 
-export const VKWidget = () => (
-    <div id="vk_groups" className={styles.root}>
-        <Script src={'https://vk.com/js/api/openapi.js?169'}  onReady={() => {
-            globalThis.VK.Widgets.Group("vk_groups", {
-              mode: 3,
-              color3: 'F7A136'
-          }, 53659209)
-        }} />
-    </div>
+export const VKWidget = () => {
+  const url = 'https://vk.com/js/api/openapi.js?169';
+  
+  const onReady = () => globalThis.VK.Widgets.Group("vk_groups", {
+          mode: 3,
+          color3: 'F7A136'
+      }, 53659209)
+
+  return (
+      <div id="vk_groups" className={styles.root}>
+          <Script src={url}  onReady={onReady} />
+      </div>
   )
+}
 
