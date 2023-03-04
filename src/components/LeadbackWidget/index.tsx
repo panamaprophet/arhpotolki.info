068fd05407e2c1264a9e6f847e53eb2afc06a444
RichardPickman <drezzerock@gmail.com>
2023-03-04 18:41:50 +0100

    add Leadback Widget script component

---
+++ src/components/LeadbackWidget/index.tsx
@@ -0,0 +1,10 @@
+import Script from "next/script";
+
+export const LeadbackWidget = () => {
+    const url = 'https://leadback.ru/js/leadback.js';
+    
+    globalThis._emv = globalThis._emv || [];
+    globalThis._emv['campaign'] = '0a656cc19f3f5b27324bfa32';
+    
+    return <Script src={url} async />;
+}
