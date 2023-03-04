bc67c127b462d41b9d091dfebaf33c9f4b6a2b54
RichardPickman <drezzerock@gmail.com>
2023-03-04 17:51:56 +0100

    add vk widget with styles

---
+++ src/components/VKWidget/index.tsx
@@ -0,0 +1,14 @@
+import Script from 'next/script';
+import styles from './styles.module.css';
+
+export const VKWidget = () => (
+    <div id="vk_groups" className={styles.root}>
+        <Script src={'https://vk.com/js/api/openapi.js?169'}  onReady={() => {
+            globalThis.VK.Widgets.Group("vk_groups", {
+              mode: 3,
+              color3: 'F7A136'
+          }, 53659209)
+        }} />
+    </div>
+  )
+
