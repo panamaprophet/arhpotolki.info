febbaa831c20794de88a4949a5c904ed95e61551
RichardPickman <drezzerock@gmail.com>
2023-03-04 18:43:47 +0100

    refactor globalThis check

--- src/components/LeadbackWidget/index.tsx
+++ src/components/LeadbackWidget/index.tsx
@@ -3,7 +3,10 @@ import Script from "next/script";
 export const LeadbackWidget = () => {
     const url = 'https://leadback.ru/js/leadback.js';
     
-    globalThis._emv = globalThis._emv || [];
+    if (!globalThis._emv) {
+        globalThis._emv = [];
+    }
+    
     globalThis._emv['campaign'] = 'leadback_campaign_id';
     
     return <Script src={url} async />;
