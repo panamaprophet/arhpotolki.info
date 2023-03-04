884b6f6700a3b72ce51f12bf5ce5a11cfcd3b438
RichardPickman <drezzerock@gmail.com>
2023-03-04 19:44:03 +0100

    change: parentheses, array to object, remove async

--- src/components/LeadbackWidget/index.tsx
+++ src/components/LeadbackWidget/index.tsx
@@ -1,13 +1,13 @@
-import Script from "next/script";
+import Script from 'next/script';
 
 export const LeadbackWidget = () => {
     const url = 'https://leadback.ru/js/leadback.js';
     
     if (!globalThis._emv) {
-        globalThis._emv = [];
+        globalThis._emv = {};
     }
     
     globalThis._emv['campaign'] = 'leadback_campaign_id';
     
-    return <Script src={url} async />;
+    return <Script src={url} />;
 }
