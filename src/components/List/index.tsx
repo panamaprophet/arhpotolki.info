45e05ff4c42d4231086cb4c88501780a7e682c47
pp <pp@rawsound.ru>
2023-03-07 11:59:32 +0100

    properly clean the admin page

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -6,12 +6,14 @@ interface Props {
     children: ReactNode,
     type?: 'default' | 'numeric' | 'bullet',
     align?: string,
+    wrap?: boolean,
 }
 
-export const List = ({ children, align = 'evenly', type = 'default' }: Props) => (
+export const List = ({ children, align = 'evenly', type = 'default', wrap = true }: Props) => (
     <ul
         className={cx(
             styles.root,
+            styles[`wrap_${wrap}`],
             styles[`align_${align}`],
             styles[`type_${type}`],
         )}
