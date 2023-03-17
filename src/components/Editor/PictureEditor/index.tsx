1e3ee2ad4a21b50fc0f7e8d2ab2d380c925c750c
pp <pp@rawsound.ru>
2023-03-17 15:10:01 +0100

    provide a possibility to upload multiple files in one go

--- src/components/Editor/PictureEditor/index.tsx
+++ src/components/Editor/PictureEditor/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 
 import { useState, useEffect } from 'react';
-import { InputFile, InputList } from '../../Input';
+import { InputList } from '../../Input';
 import Image from 'next/image';
 import { Picture } from '../../../types';
 import { Column } from '../../Layout';
@@ -10,25 +10,20 @@ import styles from './index.module.css';
 
 
 interface Props extends Partial<Picture> {
-    onCreate: (item: Omit<Picture, 'id'>) => void,
     onUpdate: (item: Picture) => void,
     onRemove: (item: Pick<Picture, 'id'>) => void,
 };
 
+const IMAGE_SIZE = 120;
 
 export const PictureEditor = (props: Props) => {
-    const { id = null, url = '', tags = [], onCreate, onUpdate, onRemove } = props;
+    const { id = null, url = '', tags = [], onUpdate, onRemove } = props;
     const [state, setState] = useState<Picture>({ id, url, tags });
     const [isSaved, setSaved] = useState(true);
 
     const onChange = (changes) => {
         setState({ ...state, ...changes });
         setSaved(false);
-    };
-
-    const _onCreate = () => {
-        onCreate(state);
-        setState({ id: null, url: '', tags: [] });
     };
 
     useEffect(() => {
@@ -40,17 +35,9 @@ export const PictureEditor = (props: Props) => {
 
     return (
         <Column>
-            {!state.url
-                ? <InputFile onUpload={url => onChange({ url })} />
-                : <Image src={state.url} alt={id || url} width="120" height="120" />
-            }
-
-            <InputList placeholder="категории (через пробел)" onChange={tags => onChange({ tags })} value={state.tags} />
-
-            {!state.id
-                ? <Button theme="green" onClick={_onCreate}>+</Button>
-                : <Button theme="orange" className={styles.buttonRemove} onClick={() => onRemove(state)}>Х</Button>
-            }
+            <Image src={state.url} alt={id} width={IMAGE_SIZE} height={IMAGE_SIZE} />
+            <InputList placeholder="категории" onChange={tags => onChange({ tags })} value={state.tags} />
+            <Button theme="orange" className={styles.buttonRemove} onClick={() => onRemove(state)}>x</Button>
         </Column>
     );
 };
