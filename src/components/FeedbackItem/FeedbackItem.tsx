455483d19db7900563cc8eca7805e2ce7f82926c
RichardPickman <drezzerock@gmail.com>
2023-03-18 19:49:06 +0100

    update styling and switch div to Image component

--- src/components/FeedbackItem/FeedbackItem.tsx
+++ src/components/FeedbackItem/FeedbackItem.tsx
@@ -1,14 +1,20 @@
 import { Feedback } from '../../types';
+import Image from 'next/image';
 import styles from './styles.module.css';
 
 export const FeedbackItem = ({ picture, author, date, text }: Omit<Feedback, 'id'>) => (
     <div className={styles.root}>
-        <div
-            className={styles.picture}
-            style={{
-                backgroundImage: `url(${picture})`,
-            }}
-        />
+        <div className={styles.picture}>
+            <Image
+                style={{ 
+                    objectFit: 'contain', 
+                    borderRadius: '6px' 
+                }}
+                src={picture}
+                alt="author" 
+                fill
+            />
+        </div>
         <div className={styles.author}>{author}</div>
         <div className={styles.date}>{date}</div>
         <div className={styles.text}>{text}</div>
