acfc78c80f1e7d8f74b4fd7ac511f076090b6970
pp <pp@rawsound.ru>
2023-03-19 02:04:38 +0100

    change stack name

--- scripts/stack.ts
+++ scripts/stack.ts
@@ -5,12 +5,15 @@ import { App } from 'aws-cdk-lib';
 import { ArhpotolkiInfoStack } from './stack.lib';
 
 
+const stackName = 'arhpotolki-info-default-stack';
+
 const app = new App();
 
 
-new ArhpotolkiInfoStack(app, 'ArhpotolkiInfoStack', {
-    env: { 
-        account: process.env.CDK_DEFAULT_ACCOUNT, 
+new ArhpotolkiInfoStack(app, stackName, {
+    stackName,
+    env: {
+        account: process.env.CDK_DEFAULT_ACCOUNT,
         region: process.env.CDK_DEFAULT_REGION,
     },
 });
