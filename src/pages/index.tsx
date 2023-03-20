70d16d4195c0787d23b8c740b735b55802d9f4ff
RichardPickman <drezzerock@gmail.com>
2023-03-20 13:29:25 +0100

    swap inlined widgets to own components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,21 +25,27 @@ import { Link } from '../components/Link';
 import { VKWidget } from '../components/VKWidget';
 import { LeadbackWidget } from '../components/LeadbackWidget';
 
-import { certificates, steps, features, menuLinks } from '../mocks/index.mock';
 import { getSettings } from '../resolvers/settings';
 import { getFeedbacks } from '../resolvers/feedback';
 import { getPictures } from '../resolvers/pictures';
 import { Feedback, Picture } from '../types';
+import { Description } from '../widgets/Description';
+import { Steps } from '../widgets/StepsList';
+import { Certificates } from '../widgets/Certificates';
+import { Advantages } from '../widgets/Advantages';
+import { HeaderWidget } from '../widgets/Header';
+import { FooterWidget } from '../widgets/Footer';
+import { FeedbacksWidget } from '../widgets/Feedbacks';
+import { CalculatorWidget } from '../widgets/Calculator';
+import { Application } from '../widgets/Application';
 
-const getFullYear = () => (new Date()).getFullYear();
-
-const formatLink = (link: string) => link.replaceAll(/^(mailto:|\/\/|http:\/\/|https:\/\/)/gi, '');
 
 interface Props {
     feedbacks: Feedback[],
     gallery: Picture[],
     settings: any,
 }
+
 
 const App = (props: Props) => {
     const {
@@ -48,91 +54,15 @@ const App = (props: Props) => {
         gallery
     } = props;
 
-    const [isFeedbackFormVisible, setFeedbackFormVisible] = useState(false);
-    const showFeedbackForm = () => setFeedbackFormVisible(true);
-    const hideFeedbackForm = () => setFeedbackFormVisible(false);
-
-    const [isMenuOpen, setMenuOpen] = useState(false);
-    const openMenu = () => setMenuOpen(true);
-    const closeMenu = () => setMenuOpen(false);
-
     const [order, setOrder] = useState({});
-
-    const onPriceChange = (calculations) => setOrder({ ...order, ...calculations });
-
-    const onOrderSubmit = (clientData) => {
-        const orderData = {
-            ...order,
-            ...clientData,
-        };
-
-        setOrder(orderData);
-
-        fetch('/api/order', {
-            method: 'POST',
-            body: JSON.stringify(orderData),
-        });
-    };
-
-    const onFeedbackSubmit = (feedback) => {
-        hideFeedbackForm();
-
-        fetch('/api/feedback', {
-            method: 'POST',
-            body: JSON.stringify(feedback),
-        });
-
-        // @todo: show confirmation via Notification component
-    };
-
 
     return (
         <>
-            <Header>
-                <Row style={{ width: '100%', justifyContent: 'space-between' }}>
-                    <Image src="/icons/eco.png" width="80" height="96" alt="" />
-                    <Row>
-                        <Subtitle color="#a6c719">
-                            {settings.phone}
-                        </Subtitle>
-                        <Button onClick={openMenu}>
-                            <Burger />
-                        </Button>
-                        <Modal isOpen={isMenuOpen} onClose={closeMenu}>
-                            <Menu links={menuLinks} onClick={closeMenu} />
-                        </Modal>
-                    </Row>
-                </Row>
+            <HeaderWidget {...settings} />
 
-                <Logo />
+            <Description />
 
-                <Row >
-                    <Subtitle color="#2484c6">
-                        {settings.headerNotification}
-                    </Subtitle>
-                    <Link href="#description">
-                        <ArrowDown />
-                    </Link>
-                </Row>
-            </Header>
-
-            <Section id="description" style={{ marginTop: 40, marginBottom: 40 }}>
-                <Text>Компания «АрхПотолки» на про­тя­же­нии шес­ти лет ус­пеш­но за­ни­ма­ет­ся про­да­жей и ус­та­нов­кой на­тяж­ных по­тол­ков, на тер­ри­то­рии всей Ар­хан­гель­ской об­ла­сти.</Text>
-                <Text>Сво­им глав­ным дос­ти­же­ни­ем мы счи­та­ем ин­ди­ви­ду­аль­ный под­ход к каж­до­му кли­ен­ту и пер­во­класс­ное ка­чес­тво про­из­во­ди­мой на­ми ра­бо­ты.</Text>
-                <Text>Од­но из глав­ных пре­и­му­ществ на­шей ком­па­нии, яв­ля­ет­ся сер­вис­ное об­слу­жи­ва­ние. Ведь на про­тя­же­нии всей га­ра­нтии мы за­бо­тим­ся о каж­дом ус­та­нов­лен­ном на­ми по­тол­ке, будь то бюд­жет­ный или экс­клю­зив­ный по­то­лок.</Text>
-            </Section>
-
-            <Section id="advantages" style={{ background: '#f7a136' }}>
-                <Title>Только у нас:</Title>
-                <List align="center">
-                    {features.map(item => (
-                        <Card key={item.url}>
-                            <Image src={item.url} alt="" width={64} height={64} />
-                            <div>{item.text}</div>
-                        </Card>
-                    ))}
-                </List>
-            </Section>
+            <Advantages />
 
             {gallery.length && (
                 <Section id="examples">
@@ -144,91 +74,18 @@ const App = (props: Props) => {
             <Separator text="Изготовление - от одного дня, монтаж - от двух часов." />
 
             {settings.prices && settings.materials && (
-                <Section id="calculator">
-                    <Title>Рассчитать стоимость:</Title>
-                    <Calculator
-                        onCalc={onPriceChange}
-                        materials={settings.materials}
-                        prices={settings.prices}
-                        lightPrice={settings.lightPrice}
-                        colorPrice={settings.colorPrice}
-                        defaultMeterPrice={settings.costBySquare}
-                    />
-                </Section>
+                <CalculatorWidget setOrder={setOrder} order={order} {...settings} />
             )}
 
-            <Section id="application" style={{ background: '#f7a136' }}>
-                <Title condenced={true}>Оставить заявку:</Title>
-                <OrderForm onSubmit={onOrderSubmit} />
-            </Section>
+            <Application order={order} setOrder={setOrder} />
 
-            <Section id="howto">
-                <Title>Как это работает?</Title>
-                <List align="center">
-                    {steps.map(item => (
-                        <Card key={item.url}>
-                            <Image src={item.url} alt="" width={64} height={64} style={{ background: '#f7a136', borderRadius: '50%' }} />
-                            <div>{item.text}</div>
-                        </Card>
-                    ))}
-                </List>
-            </Section>
+            <Steps />
 
-            <Section id="certificates">
-                <Title>Сертификаты соответствия:</Title>
-                <List align="center">
-                    {certificates.map(url => (
-                        <Card key={url}>
-                            <Image src={url} alt="" width="123" height="177" />
-                        </Card>
-                    ))}
-                </List>
-            </Section>
+            <Certificates />
 
-            <Section id="feedbacks">
-                <Title>Отзывы:</Title>
-                <Carousel>
-                    {feedbacks.map(item => (
-                        <FeedbackItem key={item.id} {...item} />
-                    ))}
-                </Carousel>
-                <Row>
-                    <Button theme="orange" onClick={showFeedbackForm}>
-                        Оставить отзыв
-                    </Button>
-                    <Modal isOpen={isFeedbackFormVisible} onClose={hideFeedbackForm}>
-                        <FeedbackForm onSubmit={onFeedbackSubmit} />
-                    </Modal>
-                </Row>
-            </Section>
+            <FeedbacksWidget feedbacks={feedbacks} />
 
-            <Footer>
-                <Column style={{ alignItems: 'center', textAlign: 'center', gap: 16 }}>
-                    <Map />
-                    <Text>
-                        <strong>{settings.companyName}</strong>
-                        <br />
-                        {settings.address}
-                        <br />
-                        {settings.phone}
-                    </Text>
-
-                    {settings.links && <Text>
-                        {settings.links.map((link) => (
-                            <div key={link}>
-                                <Link href={link} target="_blank">{formatLink(link)}</Link>
-                            </div>
-                        ))}
-                    </Text>}
-
-                    <LeadbackWidget />
-                    <VKWidget />
-
-                    <Text>
-                        Copyright © {settings.companyName}, 2014 - {getFullYear()} | <Link href="/privacy">Защита персональной информации</Link>
-                    </Text>
-                </Column>
-            </Footer >
+            <FooterWidget {...settings} />
         </>
     );
 };
