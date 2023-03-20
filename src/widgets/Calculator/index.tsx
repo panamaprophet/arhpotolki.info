153c7dfa848e10c63951a8d2d87a4920d9d94b8a
RichardPickman <drezzerock@gmail.com>
2023-03-20 14:44:38 +0100

    add check from index to widget, fix quotes and tab line

--- src/widgets/Calculator/index.tsx
+++ src/widgets/Calculator/index.tsx
@@ -1,21 +1,26 @@
-import { Calculator } from "../../components/Calculator";
-import { Section } from "../../components/Section";
-import { Title } from "../../components/Text";
+import { Calculator as Calc } from '../../components/Calculator';
+import { Section } from '../../components/Section';
+import { Title } from '../../components/Text';
 
-export const CalculatorWidget = ({ materials, prices, lightPrice, colorPrice, costBySquare, setOrder, order }) => {
-    const onPriceChange = (calculations) => setOrder({ ...order, ...calculations });
+
+export const Calculator = ({ materials, prices, lightPrice, colorPrice, costBySquare, setOrder, order }) => {
+    const onPriceChange = calculations => setOrder({ ...order, ...calculations });
+
+    if (!prices && !materials) {
+        return null;
+    }
 
     return (
-            <Section id="calculator">
-                <Title>Рассчитать стоимость:</Title>
-                <Calculator
-                    onCalc={onPriceChange}
-                    materials={materials}
-                    prices={prices}
-                    lightPrice={lightPrice}
-                    colorPrice={colorPrice}
-                    defaultMeterPrice={costBySquare}
-                />
-            </Section>
+        <Section id="calculator">
+            <Title>Рассчитать стоимость:</Title>
+            <Calc
+                onCalc={onPriceChange}
+                materials={materials}
+                prices={prices}
+                lightPrice={lightPrice}
+                colorPrice={colorPrice}
+                defaultMeterPrice={costBySquare}
+            />
+        </Section>
     );
 };
