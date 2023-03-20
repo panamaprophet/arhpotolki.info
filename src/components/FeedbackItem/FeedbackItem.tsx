21d2ec42a09304b48dc1a9dfc04f71fb7166debe
pp <pp@rawsound.ru>
2023-03-20 08:43:28 +0100

    format feedback date, move direct image styles to class and fix the content alignment

--- src/components/FeedbackItem/FeedbackItem.tsx
+++ src/components/FeedbackItem/FeedbackItem.tsx
@@ -1,22 +1,30 @@
 import { Feedback } from '../../types';
 import Image from 'next/image';
 import styles from './styles.module.css';
+import { Column } from '../Layout';
+
+
+const formatDate = (timestamp: number, locale = 'ru') => {
+    const date = new Date(timestamp);
+
+    return date.toLocaleDateString(locale);
+};
+
 
 export const FeedbackItem = ({ picture, author, date, text }: Omit<Feedback, 'id'>) => (
     <div className={styles.root}>
-        <div className={styles.picture}>
+        <div className={styles.pictureContainer}>
             {picture && <Image
-                style={{ 
-                    objectFit: 'contain', 
-                    borderRadius: '6px' 
-                }}
+                className={styles.picture}
                 src={picture}
-                alt="author" 
+                alt="author"
                 fill
             />}
         </div>
-        <div className={styles.author}>{author}</div>
-        <div className={styles.date}>{date}</div>
-        <div className={styles.text}>{text}</div>
+        <Column>
+            <div className={styles.author}>{author}</div>
+            <div className={styles.date}>{formatDate(date)}</div>
+            <div className={styles.text}>{text}</div>
+        </Column>
     </div>
 );
