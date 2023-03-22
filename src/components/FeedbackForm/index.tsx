cf6c3b04c869cf3bdf1a80a4f825f1498b97842c
RichardPickman <drezzerock@gmail.com>
2023-03-22 12:51:44 +0100

    update variables and move close button to parent

--- src/components/FeedbackForm/index.tsx
+++ src/components/FeedbackForm/index.tsx
@@ -1,42 +1,39 @@
 import { useState } from 'react';
+import { Feedback } from '../../types';
 import { ButtonWithStatus } from '../ButtonWithStatus';
 import { InputFile, InputText } from '../Input';
 import styles from './index.module.css';
 
-
 interface Props {
-    onSubmit: (data: { name: string, city: string, text: string, picture: string }) => void,
-    onClose: () => void,
+    onSubmit: (data: Omit<Feedback, 'id' | 'date'>) => void;
 }
 
-
-export const FeedbackForm = ({ onSubmit, onClose }: Props) => {
-    const [name, setName] = useState('');
+export const FeedbackForm = ({ onSubmit }: Props) => {
+    const [author, setAuthor] = useState('');
     const [city, setCity] = useState('');
     const [text, setText] = useState('');
     const [picture, setPicture] = useState('');
 
     let isDisabled = false;
 
-    if (!name.length || !city.length || !text.length) {
+    if (!author.length || !city.length || !text.length) {
         isDisabled = true;
     }
 
     return (
         <form className={styles.root}>
-            <div className={styles.closeButton} onClick={onClose}>x</div>
-            <InputText placeholder="Пожалуйста, представьтесь" value={name} onChange={setName} />
+            <InputText placeholder="Пожалуйста, представьтесь" value={author} onChange={setAuthor} />
             <InputText placeholder="Ваш город" value={city} onChange={setCity} />
             <InputText placeholder="Отзыв" value={text} onChange={setText} />
 
             <InputFile onUpload={setPicture} />
 
-            <ButtonWithStatus 
+            <ButtonWithStatus
                 theme="green"
                 disabled={isDisabled}
                 className={styles.submitButton}
-                onClick={() => onSubmit({ name, city, text, picture })}
-                status={['Отправить','Отправляется','Отправлено','Ошибка']}
+                onClick={() => onSubmit({ author, city, text, picture })}
+                status={['Отправить', 'Отправляется', 'Отправлено', 'Ошибка']}
             />
         </form>
     );
