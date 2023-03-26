d6b1b6f8fecf2e84e153bca0e518c94580afb907
RichardPickman <drezzerock@gmail.com>
2023-03-26 15:20:52 +0200

    upload image before full feedback fetch

--- src/widgets/Feedback/index.tsx
+++ src/widgets/Feedback/index.tsx
@@ -15,35 +15,58 @@ import styles from './styles.module.css';
 export const FeedbackWidget = () => {
     const { feedback: items } = useContext(Context);
 
-    const [isFeedbackFormVisible, setFeedbackFormVisible] = useState(false);
-    const showFeedbackForm = () => setFeedbackFormVisible(true);
-    const hideFeedbackForm = () => setFeedbackFormVisible(false);
+    const upload = async (file: File) => {
+        try {
+            const name = encodeURIComponent(file.name);
+            const response = await fetch(`/api/upload/${name}`);
+            const { url } = await response.json();
 
-    const onFeedbackSubmit = (feedback: Omit<Feedback, 'id' | 'date'>) =>
-        fetch('/api/feedback', {
-            method: 'POST',
-            body: JSON.stringify(feedback),
-        });
+            await fetch(url, { method: 'PUT', body: file });
 
-    return (
-        <Section id="feedback">
-            <Title>Отзывы:</Title>
-            <Carousel>
-                {items.map(item => (
-                    <FeedbackItem key={item.id} {...item} />
-                ))}
-            </Carousel>
-            <Row>
-                <Button theme="orange" onClick={showFeedbackForm}>
-                    Оставить отзыв
-                </Button>
-                <Modal size='small' isOpen={isFeedbackFormVisible} onClose={hideFeedbackForm}>
-                    <div className={styles.closeButton} onClick={hideFeedbackForm}>
-                        x
-                    </div>
-                    <FeedbackForm onSubmit={onFeedbackSubmit} />
-                </Modal>
-            </Row>
-        </Section>
-    );
-};
+            return url.split('?')[0];
+        } catch (error) {
+            console.log('error during upload', error);
+
+            return null;
+        }
+    };
+
+
+    export const FeedbacksWidget = ({ feedbacks }: Props) => {
+        const [isFeedbackFormVisible, setFeedbackFormVisible] = useState(false);
+        const showFeedbackForm = () => setFeedbackFormVisible(true);
+        const hideFeedbackForm = () => setFeedbackFormVisible(false);
+
+        const onFeedbackSubmit = async (formData: Omit<Feedback, 'id'>) => {
+            const picture = await upload(formData.picture);
+            const feedback = { ...formData, picture };
+
+
+            return fetch('/api/feedback', {
+                method: 'POST',
+                body: JSON.stringify(feedback),
+            });
+        }
+
+        return (
+            <Section id="feedback">
+                <Title>Отзывы:</Title>
+                <Carousel>
+                    {items.map(item => (
+                        <FeedbackItem key={item.id} {...item} />
+                    ))}
+                </Carousel>
+                <Row>
+                    <Button theme="orange" onClick={showFeedbackForm}>
+                        Оставить отзыв
+                    </Button>
+                    <Modal size='small' isOpen={isFeedbackFormVisible} onClose={hideFeedbackForm}>
+                        <div className={styles.closeButton} onClick={hideFeedbackForm}>
+                            x
+                        </div>
+                        <FeedbackForm onSubmit={onFeedbackSubmit} />
+                    </Modal>
+                </Row>
+            </Section>
+        );
+    };
