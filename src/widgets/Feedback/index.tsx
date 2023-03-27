4a0386e507b50a7ae03f3556c971ace4082360c9
RichardPickman <drezzerock@gmail.com>
2023-03-28 00:26:44 +0200

    change AWSUploader to InputFile and change addUrls func

--- src/widgets/Feedback/index.tsx
+++ src/widgets/Feedback/index.tsx
@@ -8,65 +8,50 @@ import { Modal } from '../../components/Modal';
 import { Section } from '../../components/Section';
 import { Title } from '../../components/Text';
 import { Context } from '../../context';
-import { Feedback } from '../../types';
+import { uploadFile } from '../../resolvers/storage';
 import styles from './styles.module.css';
 
 
 export const FeedbackWidget = () => {
     const { feedback: items } = useContext(Context);
 
-    const upload = async (file: File) => {
-        try {
-            const name = encodeURIComponent(file.name);
-            const response = await fetch(`/api/upload/${name}`);
-            const { url } = await response.json();
 
-            await fetch(url, { method: 'PUT', body: file });
+    const [isFeedbackFormVisible, setFeedbackFormVisible] = useState(false);
+    const showFeedbackForm = () => setFeedbackFormVisible(true);
+    const hideFeedbackForm = () => setFeedbackFormVisible(false);
 
-            return url.split('?')[0];
-        } catch (error) {
-            console.log('error during upload', error);
+    const onFeedbackSubmit = async (formData: { author: string, city: string, text: string, picture?: File }) => {
+        const picture = formData.picture
+            ? await uploadFile(formData.picture)
+            : null;
 
-            return null;
-        }
-    };
+        const feedback = { ...formData, picture };
 
+        return fetch('/api/feedback', {
+            method: 'POST',
+            body: JSON.stringify(feedback),
+        });
+    }
 
-    export const FeedbacksWidget = ({ feedbacks }: Props) => {
-        const [isFeedbackFormVisible, setFeedbackFormVisible] = useState(false);
-        const showFeedbackForm = () => setFeedbackFormVisible(true);
-        const hideFeedbackForm = () => setFeedbackFormVisible(false);
-
-        const onFeedbackSubmit = async (formData: Omit<Feedback, 'id'>) => {
-            const picture = await upload(formData.picture);
-            const feedback = { ...formData, picture };
-
-
-            return fetch('/api/feedback', {
-                method: 'POST',
-                body: JSON.stringify(feedback),
-            });
-        }
-
-        return (
-            <Section id="feedback">
-                <Title>Отзывы:</Title>
-                <Carousel>
-                    {items.map(item => (
-                        <FeedbackItem key={item.id} {...item} />
-                    ))}
-                </Carousel>
-                <Row>
-                    <Button theme="orange" onClick={showFeedbackForm}>
-                        Оставить отзыв
-                    </Button>
-                    <Modal size='small' isOpen={isFeedbackFormVisible} onClose={hideFeedbackForm}>
-                        <div className={styles.closeButton} onClick={hideFeedbackForm}>
-                            x
-                        </div>
-                        <FeedbackForm onSubmit={onFeedbackSubmit} />
-                    </Modal>
-                </Row>
-            </Section>
-        );
-    };
+    return (
+        <Section id="feedback">
+            <Title>Отзывы:</Title>
+            <Carousel>
+                {items.map(item => (
+                    <FeedbackItem key={item.id} {...item} />
+                ))}
+            </Carousel>
+            <Row>
+                <Button theme="orange" onClick={showFeedbackForm}>
+                    Оставить отзыв
+                </Button>
+                <Modal size='small' isOpen={isFeedbackFormVisible} onClose={hideFeedbackForm}>
+                    <div className={styles.closeButton} onClick={hideFeedbackForm}>
+                        x
+                    </div>
+                    <FeedbackForm onSubmit={onFeedbackSubmit} />
+                </Modal>
+            </Row>
+        </Section>
+    );
+};
