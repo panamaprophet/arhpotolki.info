a39a7d04412ae8e2f014126032d8b9f3eeda585f
RichardPickman <drezzerock@gmail.com>
2023-03-28 00:27:57 +0200

    add uploadFile and uploadMultiple functions

--- src/resolvers/pictures/index.ts
+++ src/resolvers/pictures/index.ts
@@ -45,3 +45,27 @@ export const getPictures = async () => {
 
     return items;
 };
+
+export const uploadFile = async (file: File): Promise<string | null> => {
+    try {
+        const name = encodeURIComponent(file.name);
+        const response = await fetch(`/api/upload/${name}`);
+        const { url } = await response.json();
+
+        await fetch(url, { method: 'PUT', body: file });
+
+        return url.split('?')[0];
+    } catch (error) {
+        console.log('error during upload', error);
+
+        return null;
+    }
+};
+
+export const uploadMultiple = async (files: FileList) => {
+    const fileList = Array.from(files);
+    const promises = fileList.map(uploadFile);
+    const urls = await Promise.all(promises);
+
+    return urls;
+};
