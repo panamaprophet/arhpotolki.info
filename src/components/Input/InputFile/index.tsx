bddcfbef287f9e5bfaf6219bb8bbed5c7cdebf31
RichardPickman <drezzerock@gmail.com>
2023-04-01 20:36:25 +0200

    add file size validator

--- src/components/Input/InputFile/index.tsx
+++ src/components/Input/InputFile/index.tsx
@@ -1,4 +1,5 @@
-import { ChangeEventHandler } from 'react';
+import { ChangeEventHandler, useState } from 'react';
+import { cx } from '../../../helpers';
 import styles from './index.module.css';
 
 
@@ -7,8 +8,11 @@ interface Props {
     onChange: (files: File[]) => void,
 };
 
+const megabyteSize = 1049000;
+
 
 export const InputFile = ({ multiple = false, onChange }: Props) => {
+    const [isError, setIsError] = useState(false);
 
     const _onChange: ChangeEventHandler<HTMLInputElement> = (event) => {
         if (!event.target.files) {
@@ -16,21 +20,37 @@ export const InputFile = ({ multiple = false, onChange }: Props) => {
             return;
         }
 
+        if (event.target.files[0].size > megabyteSize) {
+            setIsError(true);
+
+            setTimeout(() => setIsError(false), 4200);
+
+            return;
+        }
+
         onChange(Array.from(event.target.files));
     }
 
     return (
-        <label className={styles.root}>
+        <label className={cx(styles.root, isError && styles.error)}>
             <input
                 type="file"
                 multiple={multiple}
                 className={styles.input}
                 onChange={_onChange}
             />
-            <div className={styles.meta}>
-                <div>Прикрепить файл</div>
-                <div className={styles.text}>Перетащите сюда или нажмите для выбора</div>
-            </div>
+            {isError && (
+                <div className={styles.meta}>
+                    <div>Ошибка при загрузке</div>
+                    <div className={styles.text}>Выбранный файл превышает допустимый размер в 1 мегабайт</div>
+                </div>
+            )}
+            {!isError && (
+                <div className={styles.meta}>
+                    <div>Прикрепить файл</div>
+                    <div className={styles.text}>Перетащите сюда или нажмите для выбора</div>
+                </div>
+            )}
         </label>
     );
 };
