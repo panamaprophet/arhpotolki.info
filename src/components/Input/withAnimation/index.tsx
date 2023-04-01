3cf8e41464ac48f9787810db9f3d0fc7b2f41c4c
sergei klykov <pp@rawsound.ru>
2023-04-01 18:19:04 +0200

    show some feedback when input onChange is fired

---
+++ src/components/Input/withAnimation/index.tsx
@@ -0,0 +1,28 @@
+import { ComponentType, useState } from 'react'
+import { cx } from '../../../helpers';
+import styles from './index.module.css';
+
+export const withAnimation = <P extends object>(Component: ComponentType<P>) => {
+    const WrappedComponent = (props: P) => {
+        const [isChanged, setChanged] = useState(false);
+
+        const classes = cx(
+            styles.root,
+            isChanged && styles.root_animated
+        );
+
+        const onChange = (value: unknown) => {
+            setChanged(true);
+            // @ts-ignore
+            props.onChange(value);
+        };
+
+        return (
+            <div className={classes} onAnimationEnd={() => setChanged(false)}>
+                <Component {...props} onChange={onChange} />
+            </div>
+        )
+    }
+
+    return WrappedComponent;
+};
