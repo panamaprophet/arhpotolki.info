543603af23d48cedf507dc963ce9e0dfd829a28d
RichardPickman <drezzerock@gmail.com>
2023-04-01 20:09:58 +0200

    update icons

--- src/components/Icon/ArrowLeft.tsx
+++ src/components/Icon/ArrowLeft.tsx
@@ -1,12 +1,7 @@
 export const ArrowLeft = ({ color = '#f7a136', size = 32 }: { color?: string, size?: number }) => (
-    <svg
-        viewBox="0 0 40 40"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        style={{ width: size, height: size }}
-    >
-        <path d="M19 25H17L12.5 20L17 15H19L14.5 20L19 25Z" fill={color} />
-        <path d="M25 25H23L18.5 20L23 15H25L20.5 20L25 25Z" fill={color} />
-        <circle cx="20" cy="20" r="19.5" stroke-width="1.4" stroke={color} />
+    <svg viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ width: size, height: size }}>
+        <path d="M26.5 25H24.5L20 20L24.5 15H26.5L22 20L26.5 25Z" fill={color} />
+        <path d="M20.5 25H18.5L14 20L18.5 15H20.5L16 20L20.5 25Z" fill={color} />
+        <circle cx="20" cy="20" r="19" stroke={color} stroke-width="2" />
     </svg>
 );
--- src/components/Icon/ArrowRight.tsx
+++ src/components/Icon/ArrowRight.tsx
@@ -1,12 +1,7 @@
 export const ArrowRight = ({ color = '#f7a136', size = 32 }: { color?: string, size?: number }) => (
-    <svg
-        viewBox="0 0 40 40"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        style={{ width: size, height: size }}
-    >
-        <path d="M21 15H23L27.5 20L23 25H21L25.5 20L21 15Z" fill={color} />
-        <path d="M15 15H17L21.5 20L17 25H15L19.5 20L15 15Z" fill={color} />
+    <svg viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ width: size, height: size }}>
+        <path d="M20 15H22L26.5 20L22 25H20L24.5 20L20 15Z" fill={color} />
+        <path d="M14 15H16L20.5 20L16 25H14L18.5 20L14 15Z" fill={color} />
         <circle cx="20" cy="20" r="19" stroke={color} stroke-width="2" />
     </svg>
 );
